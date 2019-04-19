6535b7a0ba26eb5f88dfcb4cc0c72e26424fdd32
yushiwo <zhengrui@otcbtc.com>
2019-04-19 13:49:50 +0800

    确定主页需要展示的一些功能

--- lib/modules/home/MainPage.dart
+++ lib/modules/home/MainPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_free_wifi/modules/global/util/Util.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 /*
 * 首页，九宫格显示各个免费小工具（用ListView实现，支持滑动）
@@ -6,9 +8,84 @@ import 'package:flutter/material.dart';
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("九宫格显示各个免费小工具（用ListView实现，支持滑动）"),
-    );
+    return new Padding(
+        padding: const EdgeInsets.only(top: 12.0),
+        child: new StaggeredGridView.count(
+          crossAxisCount: 4,
+          staggeredTiles: _staggeredTiles,
+          children: _tiles,
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+          padding: const EdgeInsets.all(4.0),
+        ));
   }
   
+}
+
+
+List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(2, 2),
+];
+
+List<Widget> _tiles = const <Widget>[
+  const _Example01Tile(Colors.green, Icons.highlight, "手电筒"),
+  const _Example01Tile(Colors.lightBlue, Icons.wifi, "附近Wi-Fi"),
+  const _Example01Tile(Colors.amber, Icons.phone_android, "手机号码归属地"),
+  const _Example01Tile(Colors.brown, Icons.library_books, "新闻头条"),
+  const _Example01Tile(Colors.deepOrange, Icons.history, "历史上的今天"),
+  const _Example01Tile(Colors.indigo, Icons.child_care, "笑话"),
+];
+
+class _Example01Tile extends StatelessWidget {
+  const _Example01Tile(this.backgroundColor, this.iconData, this.iconName);
+
+  final Color backgroundColor;
+  final IconData iconData;
+  final String iconName;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      color: backgroundColor,
+      child: new InkWell(
+        onTap: () {
+          Util.showToast(iconName);
+        },
+        child: new Center(
+          child: new Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new Center(
+              child: new Container(
+                child: getItemContentView(),
+              ),
+            )
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getItemContentView() {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Icon(
+          iconData,
+          color: Colors.white,
+        ),
+        new Padding(
+          padding: new EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 0.0),
+          child: new Text(
+              iconName,
+              style: new TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.bold)
+          ),
+        ),
+      ],
+    );
+  }
 }
